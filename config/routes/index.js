a2f0564ad280805d2069d4886e2e3931606b6273
clementRbl <clement@snapkey.fr>
2021-02-24 17:17:46 +0100

    last initialization

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,6 +1,10 @@
 module.exports = (express, controllers) => {
-    const routes = [
+    /* const book_route = require('./book') */
+    const user_route = require('./user');
 
+    const routes = [
+        /* book_route(express, controllers) */
+        user_route(express, controllers)
     ]
 
     return routes
---
+++ controllers/user.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+  /* const user_controller = {
+    
+  } */
+}
