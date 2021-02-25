3f6e0c1e9e2b5e04025536cb886fbb3bb6b32e44
yoramtaieb <yoram.hob@outlook.fr>
2021-02-25 15:24:08 +0100

    GetAllBooks

--- db/models/index.js
+++ db/models/index.js
@@ -1,12 +1,13 @@
-const db = require('../config');
+// models dépendency
+const db = require("../config");
 
 // models
-const User = require('./user');
+const Book = require("./book");
 
 // create a object models for map all the models
 const models = {
-    user: new User(db)
-}
+  book: new Book(db),
+};
 
 // export our model object
 module.exports = models;
