13d4601761ed9b8da6cc076aabfdbf3bd72152c6
Amahaya <babisko113@yahoo.fr>
2021-02-26 11:21:21 +0100

    MPD redone & Bdd created

--- db/db.sql
+++ db/db.sql
@@ -13,11 +13,30 @@ CREATE TABLE IF NOT EXISTS Category (
   name VARCHAR(55)
 );
 
+CREATE TABLE IF NOT EXISTS Genre (
+  id INT(11) PRIMARY KEY NOT NULL,
+  name VARCHAR(55)
+);
+
+CREATE TABLE IF NOT EXISTS BookCategory (
+  id INT(11) PRIMARY KEY NOT NULL,
+  idBook INT(11),
+  idCategory INT(11),
+  FOREIGN KEY(idBook) REFERENCES Book(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY(idCategory) REFERENCES Category(id)
+  ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS Book (
   id INT(11) PRIMARY KEY NOT NULL,
   name VARCHAR(55),
-  idCategory INT(11),
-  FOREIGN KEY (idCategory) REFERENCES Category(id)
+  idGenre INT(11),
+  idBookCategory INT(11),
+  author VARCHAR(55),
+  FOREIGN KEY (idBookCategory) REFERENCES BookCategory(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY(idGenre) REFERENCES Genre(id)
   ON DELETE CASCADE
 );
 
@@ -25,7 +44,8 @@ CREATE TABLE IF NOT EXISTS Rental (
   id INT(11) PRIMARY KEY NOT NULL,
   idUser INT(11),
   idBook INT(11),
-  date DATETIME,
+  dateRent DATETIME,
+  dateReturn DATETIME,
   FOREIGN KEY (idUser) REFERENCES User(id)
   ON DELETE CASCADE,
   FOREIGN KEY (idBook) REFERENCES Book(id)
@@ -36,6 +56,8 @@ CREATE TABLE IF NOT EXISTS Wishlist (
   id INT(11) PRIMARY KEY NOT NULL,
   idUser INT(11),
   idBook INT(11),
+  dateCreate DATETIME,
+  dateDel DATETIME,
   FOREIGN KEY (idUser) REFERENCES User(id)
   ON DELETE CASCADE,
   FOREIGN KEY (idBook) REFERENCES Book(id)
@@ -44,12 +66,29 @@ CREATE TABLE IF NOT EXISTS Wishlist (
 
 INSERT INTO Category (`id`, `name`) VALUES
 ('1', 'Action'),
-('2', 'Roman'),
+('2', 'Romance'),
 ('3', 'Policier'),
 ('4', 'Science-fiction');
 
-INSERT INTO Book (`id`, `name`, `idCategory`) VALUES
-('1', 'Les maladies', '2'),
-('2', 'Arsene Lupin', '3'),
-('3', 'Retour vers le futur', '4'),
-('4', 'Mission Impossible', '1');
+INSERT INTO Genre (`id`, `name`) VALUES
+('1', 'Roman'),
+('2', 'BD'),
+('3', 'Manga'),
+('4', 'Poesie');
+
+
+INSERT INTO BookCategory (`id`, `idBook`, `idCategory`) VALUES
+('1', '1', '1'),
+('2', '1', '4'),
+('3', '2', '1'),
+('4', '3', '3'),
+('5', '4', '2'),
+('6', '4', '1'),
+('7', '4', '3');
+
+
+INSERT INTO Book (`id`, `name`, `idGenre`, `idBookCategory`,`author`) VALUES
+('1', 'Les maladies', '2', '1', 'My' ),
+('2', 'Arsene Lupin', '3', '3','Yoram'),
+('3', 'Retour vers le futur', '4', '2', 'Clement'),
+('4', 'Mission Impossible', '1', '4', 'Inconnu');
