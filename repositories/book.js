dbe425e265751bed435e1658b72546365fe405f6
Amahaya <babisko113@yahoo.fr>
2021-02-26 15:59:20 +0100

    getByName Book

--- repositories/book.js
+++ repositories/book.js
@@ -6,6 +6,11 @@ module.exports = (models) => {
     getById: async (id) => {
       return models.book.query("select * from Book where id = ? LIMIT 1", [id]);
     },
+    getByName: async (name) => {
+      return models.book.query("select * from Book where name = ? LIMIT 1", [
+        name,
+      ]);
+    },
   };
 
   return book_repository;
